b553a0f657a0d9078d4d03606d9d7cd2c61040b6
Kimmy <dellejero.km@obmontessori.edu.ph>
2025-05-08 11:16:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function displayLevelInfo(){
+	var levelEntry = Number(document.getElementById('levelInput').value);
+
+		switch (levelEntry) {
+			case 1:
+				document.getElementById('displayLevel').innerHTML =
+				"Level: Easy Mode";
+				break;
+			case 2: 
+				document.getElementById('displayLevel').innerHTML =
+				"Level: Normal Mode";
+				break;
+			case 3:
+				document.getElementById('displayLevel').innerHTML = 
+				"Level: Difficult Mode";
+				break;
+			default: 
+				window.alert("Invalid Section Number");
+				}
+}
